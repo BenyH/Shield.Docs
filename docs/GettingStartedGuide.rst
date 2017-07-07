6578d027fb9decaabbd8ea2d196051916ec21d4c
ErezPasternak <erez.pasternak@gmail.com>
2017-07-07 15:02:11 +0300

    more fixes

--- docs/GettingStartedGuide.rst
+++ docs/GettingStartedGuide.rst
@@ -30,7 +30,7 @@ Key Features
 ------------
 
 Ericom Shield has the following key features:
-*	Enables users to browse the internet safely and securely while isolating the users’ machine and the internal network from threats of malware and ransomware.
-*	Seamless user experience with zero installation on the end point devices – users benefit from the same experience as a non-protected browser, but without the risks.
-*	Rich web based administration console, supported by all browsers.  The Admin interface does not require any high maintenance plug-ins or installers. 
-*   Includes a policy engine to allow Administrators to easily manage which sites users can access, download files, store cookies etc. 
+- Enables users to browse the internet safely and securely while isolating the users’ machine and the internal network from threats of malware and ransomware.
+- Seamless user experience with zero installation on the end point devices – users benefit from the same experience as a non-protected browser, but without the risks.
+- Rich web based administration console, supported by all browsers.  The Admin interface does not require any high maintenance plug-ins or installers. 
+- Includes a policy engine to allow Administrators to easily manage which sites users can access, download files, store cookies etc. 
--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Ericom Shield'
+copyright = u'2017, Ericom'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ReadtheDocsTemplate.tex', u'Ericom Shield Documentation',
+   u'Ericom Shield', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'readthedocstemplate', u'Ericom Shield Documentation',
+     [u'Ericom Shield'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,7 +240,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
+  ('index', 'ReadtheDocsTemplate', u'Ericom Shield Documentation',
    u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
